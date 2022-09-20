257ab8db647f102193af12ce5d27576e71cba71a
Saugat Awale <philosh@Saugats-MacBook-Pro.local>
2022-09-20 22:57:22 +0300

    Retrieved data from four exchanges

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ const port = 3000;
 const requestMarketData = () => {
   const ftxQuotes = fetch(CONFIG.MARKET.FTX.APIURL + "markets")
     .then((res) => res.json())
-    .then((data) =>
-      data.result
+    .then((data) => ({
+      ftxQuotes: data.result
         .map((e) => ({ name: e.name, last: e.last, bid: e.bid, ask: e.ask }))
         .filter((e) => e.name.includes("USD"))
         .filter((e) => !e.name.includes("USDT"))
@@ -21,7 +21,8 @@ const requestMarketData = () => {
           );
         })
         .filter((e) => e.name.split("/")[0] !== "USD")
-    );
+        .map((e) => ({ ...e, name: e.name.split("/").join("") })),
+    }));
 
   const krakenQuotes = fetch(CONFIG.MARKET.KRAKEN.APIURL + "Assets")
     .then((res) => res.json())
@@ -40,10 +41,23 @@ const requestMarketData = () => {
         );
       });
 
-      return Promise.all(krakenPromises).then((res) => res.filter((e) => e));
+      return Promise.all(krakenPromises).then((res) => ({
+        krakenQuotes: res
+          .filter((e) => e)
+          .map((e) => {
+            const tickerName = Object.keys(e)[0];
+            const tickerObj = e[tickerName];
+            return {
+              name: tickerName,
+              last: tickerObj.c[0],
+              bid: tickerObj.b[0],
+              ask: tickerObj.a[0],
+            };
+          }),
+      }));
     });
 
-  const cointigerQuotes = fetch(
+  const coinTigerQuotes = fetch(
     CONFIG.MARKET.COINTIGER.APIURL + "market/detail"
   )
     .then((res) => {
@@ -53,7 +67,19 @@ const requestMarketData = () => {
       }
       return res.json();
     })
-    .then((data) => data)
+    .then((data) => {
+      const tickerNames = Object.keys(data);
+      const coinTigerQuotes = tickerNames.map((tickerName) => {
+        const tickerObj = data[tickerName];
+        return {
+          name: tickerName,
+          last: tickerObj.last,
+          bid: tickerObj.highestBid,
+          ask: tickerObj.lowestAsk,
+        };
+      });
+      return { coinTigerQuotes };
+    })
     .catch(console.log);
 
   const mexcQuotes = fetch(
@@ -66,77 +92,78 @@ const requestMarketData = () => {
       }
       return res.json();
     })
-    .then((data) =>
-      data.data.filter(
-        (e) =>
-          e.symbol.includes("USDT") &&
-          !e.symbol.includes("3L") &&
-          !e.symbol.includes("3S") &&
-          !e.symbol.includes("4L") &&
-          !e.symbol.includes("4S")
-      )
-    )
+    .then((data) => ({
+      mexcQuotes: data.data
+        .filter(
+          (e) =>
+            e.symbol.includes("USDT") &&
+            !e.symbol.includes("3L") &&
+            !e.symbol.includes("3S") &&
+            !e.symbol.includes("4L") &&
+            !e.symbol.includes("4S")
+        )
+        .map((e) => ({
+          name: e.symbol.split("_").join(""),
+          last: e.last,
+          bid: e.bid,
+          ask: e.ask,
+        })),
+    }))
     .catch(console.log);
-  // Promise.all([krakenQuotes, mexcQuotes]).then((res) =>
-  //   res[0].forEach((krakenQuote) => {
-  //     const krakenName = Object.keys(krakenQuote)[0];
-  //     res[1].forEach((mexcQuote) => {
-  //       if (mexcQuote.symbol.split("_")[0] === krakenName.split("USD")[0]) {
-  //         const gain1 =
-  //           (Math.abs(krakenQuote[krakenName].c[0] - mexcQuote.last) /
-  //             mexcQuote.last) *
-  //           100;
-  //         const gain2 =
-  //           (Math.abs(mexcQuote.last - krakenQuote[krakenName].c[0]) /
-  //             krakenQuote[krakenName].c[0]) *
-  //           100;
 
-  //         if (gain1 > 1 || gain2 > 1) {
-  //           console.log("--------");
-  //           console.log("gain1", gain1, "gain2", gain2);
-  //           console.log("common names", krakenName);
-  //           console.log("mexcQuote", mexcQuote);
-  //           console.log("krakenQuote", krakenQuote);
-  //           console.log("--------");
-  //         }
-  //       }
-  //     });
-  //   })
-  // );
+  Promise.all([ftxQuotes, krakenQuotes]).then((res) => {
+    displayArbitrage(res);
+  });
+};
 
-  Promise.all([cointigerQuotes, mexcQuotes]).then((res) => {
-    const coinKeys = Object.keys(res[0]);
-    res[1].forEach((mexcQuote) => {
-      const mexcName = mexcQuote.symbol.split("_")[0];
-      coinKeys.forEach((coinName) => {
-        if (coinName.includes(mexcName)) {
-          const gain1 =
-            ((mexcQuote.bid - res[0][coinName].lowestAsk) /
-              res[0][coinName].lowestAsk) *
-            100;
-          const gain2 =
-            ((res[0][coinName].highestBid - mexcQuote.ask) / mexcQuote.ask) *
-            100;
+const displayArbitrage = (marketsQuotes) => {
+  for (let i = 0; i < marketsQuotes.length; i++) {
+    const marketQuotesObj1 = marketsQuotes[i];
+    const marketName1 = Object.keys(marketQuotesObj1)[0];
+    for (let j = i + 1; j < marketsQuotes.length; j++) {
+      const marketQuotesObj2 = marketsQuotes[j];
+      const marketName2 = Object.keys(marketQuotesObj2)[0];
+      marketQuotesObj1[marketName1].forEach((marketQuoteObj1) => {
+        marketQuotesObj2[marketName2].forEach((marketQuoteObj2) => {
+          const compMarketName1 = marketName1.slice(0, -1);
+          const compMarketName2 = marketName2.slice(0, -1);
+          if (
+            marketQuoteObj1.name === marketQuoteObj2.name ||
+            compMarketName1 === marketQuoteObj2.name ||
+            compMarketName2 === marketQuoteObj1.name
+          ) {
+            console.log(
+              "common ",
+              marketName1,
+              compMarketName1,
+              marketName2,
+              compMarketName2
+            );
+            const price1 =
+              ((marketQuoteObj2.bid - marketQuoteObj1.ask) /
+                marketQuoteObj1.ask) *
+              100;
+            const price2 =
+              ((marketQuoteObj1.bid - marketQuoteObj2.ask) /
+                marketQuoteObj2.ask) *
+              100;
 
-          if (gain1 > 5 && gain1 < 200) {
-            console.log("-------");
-            console.log("common name", mexcName);
-            console.log("gain1", gain1);
-            console.log("buy cointiger :", res[0][coinName].lowestAsk);
-            console.log("sell mexc : ", mexcQuote.bid);
-            console.log("--------");
-          } else if (gain2 > 5 && gain2 < 200) {
-            console.log("------");
-            console.log("common name", mexcName);
-            console.log("gain2", gain2);
-            console.log("buy mexc : ", mexcQuote.ask);
-            console.log("sell cointiger : ", res[0][coinName].highestBid);
-
-            console.log("---------");
+            if ((price1 > 2 && price1 < 5) || (price2 > 2 && price2 < 5)) {
+              console.log("-------");
+              console.log("market1: ", marketName1, ", market2: ", marketName2);
+              console.log(marketQuoteObj1, marketQuoteObj2);
+              console.log("price1 : ", price1, " price2 : ", price2);
+              console.log("-------");
+            }
           }
-        }
+        });
       });
-    });
+    }
+  }
+  marketsQuotes.forEach((marketQuotes) => {
+    const marketName = Object.keys(marketQuotes)[0];
+    console.log("marketName", marketName);
+    console.log(marketQuotes[marketName][0]);
   });
 };
 
